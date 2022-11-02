6125f246fce5731d33bd93274d72358160a92e12
unknown <Savioanjos2020@gmail.com>
2022-11-01 22:27:04 -0300

    Iniciando com native-base

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NativeBaseProvider, Text, Center } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NativeBaseProvider>
+    <Center flex={1} bgColor="black">
+
+      <Text color="white" fontSize="24">
+        Hello React Native!
+        </Text>
+
       <StatusBar style="auto" />
-    </View>
+    </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
