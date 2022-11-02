581701edafd46727ba4ceb5adeccf41f5c4cd439
unknown <Savioanjos2020@gmail.com>
2022-11-01 23:07:56 -0300

    Finalizando primeiro dia de aula

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
 
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
-    <Center flex={1} bgColor="gray.900">
+      <StatusBar
+       barStyle="light-content"
+       backgroundColor="transparent"
+       translucent
+      />
 
-      <Text color="white" fontSize="24">
-        Hello React Native!
-        </Text>
+      {fontsLoaded ? <SignIn/> : <Loading/>}
 
-      <StatusBar style="auto" />
-    </Center>
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,14 @@
+import { Center, Text } from "native-base";
+import React from "react";
+import { Loading } from "../components/Loading";
+
+export function SignIn() {
+    return (
+    
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24} fontFamily="heading">
+        SignIn
+      </Text>
+    </Center>
+    )
+}
