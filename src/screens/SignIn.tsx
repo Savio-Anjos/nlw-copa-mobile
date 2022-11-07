ea230d379d684a56ec02e7fb28dfd7ce395b5e49
unknown <Savioanjos2020@gmail.com>
2022-11-06 21:00:55 -0300

    Configurando projeto para utilizar SVG

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,13 @@
-import { Center, Text } from "native-base";
+import { Center, Text } from 'native-base';
 import React from "react";
-import { Loading } from "../components/Loading";
+
+import Logo from '../assets/logo.svg';
 
 export function SignIn() {
     return (
     
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24} fontFamily="heading">
-        SignIn
-      </Text>
+         <Logo width={212} height={40}/>
     </Center>
     )
 }
